a53be25826524984dc30bae5c8f2667e35c72325
mazusu <qweghj1245@gmail.com>
2020-04-12 21:25:22 +0800

    init

---
+++ src/my_bot.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('Connect as' + client.user.tag);
+  client.user.setActivity('動物森友會', { type: 'PLAYING'});
+  client.guilds.cache.forEach(guild => {
+    console.log(guild.name);
+    guild.channels.cache.forEach(channel => {
+      console.log(`${channel.name} -- ${channel.type} -- ${channel.id}`);
+    })
+  });
+  // general channel 698861048593252383
+  let generalChannel = client.channels.cache.get('698861048593252383');
+  let attachment = new Discord.MessageAttachment('https://i.imgur.com/w3duR07.png');
+  // generalChannel.send(attachment);
+});
+
+client.on('message', (receiveMessage) => {
+  if (receiveMessage.author === client.user) {
+    return;
+  }
+  // receiveMessage.channel.send(`Message received, ${receiveMessage.author.toString()}: ${receiveMessage.content}`);
+  // receiveMessage.react('👍');
+
+  if (receiveMessage.content.startsWith('!')) {
+    processCommand(receiveMessage);
+  }
+});
+
+const processCommand = (receiveMessage) => {
+  let fullCommand = receiveMessage.content.substr(1);
+  let splitCommand = fullCommand.split(' ');
+  let primaryCommand = splitCommand[0];
+  let args = splitCommand.slice(1);
+
+  if (primaryCommand === 'help') {
+    helpCommand(args, receiveMessage);
+  }
+}
+
+const helpCommand = (args, receiveMessage) => {
+  if (args.length === 0) {
+    return receiveMessage.channel.send('I dont know what u want to ask, please use !help [topic]');
+  } else {
+    console.log(args);
+    return receiveMessage.channel.send(`I think you want to know ${args}`);
+  }
+}
+
+client.login('Njk4ODYxMzQ5Njg1NDI4MjU1.XpL_dw.AU76RokyEHPCPmnCJkIASHaMklw');
